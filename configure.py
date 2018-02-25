a226a1426676b1973ded88db173214dc1bcc6777
Thiago Vieira <thiago@thiagovieira.adv.br>
2018-02-25 20:53:16 -0300

    str.isalpha()

--- configure.py
+++ configure.py
@@ -12,17 +12,21 @@ def get_new_words_and_hints():
     over = False
     while not over:
         word_and_hint = []
-        new_word = input("Digite uma nova palavra:")
-        new_hint = input("Digite a dica: ")
-        word_and_hint.append(new_word)
-        word_and_hint.append(new_hint)
-        write_csv(word_and_hint)
-        option = input("\nPalavras adicionadas com sucesso. "
-                       "Digite S para continuar ou N para sair.\n").upper()
-        if option == "S":
+        new_word = input("\nDigite uma nova palavra:").strip()
+        if not str.isalpha(new_word):
+            print("\nError: Só letras são aceitas. \n")
             continue
         else:
-            over = True
+            new_hint = input("Digite a dica: ")
+            word_and_hint.append(new_word)
+            word_and_hint.append(new_hint)
+            write_csv(word_and_hint)
+            option = input("\nPalavras adicionadas com sucesso. "
+                           "Deseja continua ? (S/N): \n").upper()
+            if option == "S":
+                continue
+            else:
+                over = True
 
 def write_csv(word_and_hint):
     """Write New Word and Hint on file."""
@@ -37,6 +41,7 @@ def write_csv(word_and_hint):
 
 
 def creating_csv():
+    """Creating words_and_hints.csv with one word"""
     with open("words_and_hints.csv", "w") as new_file:
         csv_writerow = csv.writer(new_file, delimiter='|')
         amor = ['amor', 'sentimento']
